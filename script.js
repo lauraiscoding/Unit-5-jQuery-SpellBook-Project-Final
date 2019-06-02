e967ce1a58e62d6287466a6f66c0a39d72e8491e
laura <43542652+lauraiscoding@users.noreply.github.com>
2019-06-02 14:16:22 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$("#lady").click(function(){
+$("#lady").attr("src","https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/FemaleLoki.PNG/170px-FemaleLoki.PNG"); 
+});
+
+
+
+//Spell #2
+
+$(".clone").click(function(){
+ $(".blueclone").fadeIn();
+  $(".redclone").fadeIn();
+});
+
+
+
+//Spell #3
+
+$("button").click(function(){
+ $(".hammer").attr("src" ,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGK4RUCt1hTpuOpeWU--bGyQxf0y9OL9XRyYod2l4R0zo5QD4o");
+});
+
+
+ 
+$(".blueclone").hide();
+  $(".redclone").hide();
